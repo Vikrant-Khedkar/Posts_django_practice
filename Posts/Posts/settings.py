e1b86a0526ca49fe60183aaf0074e9442c31a89e
Vikrant Khedkar <vikrantkhedkar2720@gmail.com>
2022-10-26 17:42:57 +0530

    Connected Postgresql as a database

--- Posts/Posts/settings.py
+++ Posts/Posts/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'Posts.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': '0HS6RnvlzOygyXvClwe8',
+        'HOST': 'containers-us-west-48.railway.app',
+        'PORT': '6822',
     }
 }
 
